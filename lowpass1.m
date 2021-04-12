db245f595e5f47d7d4cfef55c4664bca79c5f0f7
wwtse <tsengmign@outlook.com>
2021-04-12 18:57:53 -0400

    add save function to output the filterd data .mat

--- lowpass1.m
+++ lowpass1.m
@@ -1,7 +1,8 @@
 clear;
 close all;
 %% load
-load("IMU_Hover/RSdataMine.mat");
+fname = 'RSdataMine';
+load("IMU_Hover/"+fname+".mat");
 
 t = rt_sensor.time;
 T = t(2)- t(1);
@@ -12,12 +13,12 @@ ay = rt_sensor.signals.values(:,2);
 az = rt_sensor.signals.values(:,3);
 
 %% filter 
-fx1 = filter(tfir2,1,ax);
-fy1 = filter(tfir2,1,ay);
-fz1 = filter(tfir2,1,az);
+fx1 = filter(tfir1,1,ax);
+fy1 = filter(tfir1,1,ay);
+fz1 = filter(tfir1,1,az);
 
 %% cut linear delay
-fdelay  = mean(grpdelay(tfir2,fs,length(t)));
+fdelay  = mean(grpdelay(tfir1,fs,length(t)));
 tt = t(1:end-fdelay);
 
 axx = ax(1:end-fdelay);
@@ -53,3 +54,6 @@ plot(tt,azz);
 plot(tt,fz1_1);
 title("az")
 %}
+
+%% output
+save(fname(7:end)+"facc"+".mat",'tt','fx1_1','fy1_1','fz1_1');
